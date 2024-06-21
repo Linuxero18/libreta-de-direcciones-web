3f45f15c5eb314b7105151650d73d91a153fef52
Linuxero18 <piterm555perez@gmail.com>
2024-06-21 11:28:01 -0500

    se genero ruta privada

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes}  from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import DashboardPage from "../pages/DashboardPage";
+import PrivateRoute from "./PrivateRoute";
 
 export default function AppRouter() {
     return <>
         <Routes>
             <Route index element={<LoginPage/>} />
-            <Route path="dashboard" element={<DashboardPage/>} />
+            <Route path="dashboard" element={<PrivateRoute>
+                <DashboardPage/>
+            </PrivateRoute>} />
         </Routes>
     </>;
 }
--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,20 +1,7 @@
-export default function PrivateRoute({ children, ...rest }) {
-  const token = localStorage.getItem('token');
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        token ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: location }
-            }}
-          />
-        )
-      }
-    />
-  );
+import { useLocation } from "react-router-dom";
+
+export default function PrivateRoute({ children }) {
+  const { state } = useLocation();
+
+  return state?.logged ? children : <Navigate to='/' />
 }
