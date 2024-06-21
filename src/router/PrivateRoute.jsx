e9b4dc3cf58fe9b940c596d940046a82f03fae29
Linuxero18 <piterm555perez@gmail.com>
2024-06-21 11:02:49 -0500

    nuevo commit

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+export default function PrivateRoute({ children, ...rest }) {
+  const token = localStorage.getItem('token');
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        token ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
