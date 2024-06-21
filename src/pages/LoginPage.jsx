ec6c5ab60b44277bae41d0e619fa01eee849ef0f
Linuxero18 <piterm555perez@gmail.com>
2024-06-21 02:03:55 -0500

    LoginPage modificado

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,31 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './estilos/LoginPage.css';
 import React from 'react';
 
 export default function LoginPage() {
     return (
-        <section className="vh-100 gradient-custom">
-          <div className="container py-5 h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-              <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                <div className="card text-white" style={{ borderRadius: '1rem',  maxWidth: '400px', maxHeight: '500px', backgroundColor: '#514EE6'}}>
-                  <div className="card-body p-5 text-center">
-                    <div className="mb-md-5 mt-md-4 pb-5">
-                      <p className="text-black-60 mb-4">Por favor ingrese sus credenciales para acceder al sistema.</p>
-                      <div data-mdb-input-init className="form-outline form-white mb-4">
-                        <label className="form-label" htmlFor="typeEmailX">Usuario</label>
-                        <input type="text" id="typeEmailX" className="form-control form-control-lg" />
-                      </div>
-                      <div data-mdb-input-init className="form-outline form-white mb-4">
-                        <label className="form-label" htmlFor="typePasswordX">Contraseña</label>
-                        <input type="password" id="typePasswordX" className="form-control form-control-lg" />
-                      </div>
-                      <button data-mdb-button-init data-mdb-ripple-init className="btn btn-outline-light btn-lg px-8" type="submit"><a href="/dashboard">Ingresar</a></button>
+      <>
+        <div id="template-bg-1">
+          <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+            <div className="card p-4 text-light bg-dark mb-5">
+              <div className="card-header">
+                <h3>Iniciar sesión</h3>
+              </div>
+              <div className="card-body w-100">
+                <form name="login">
+                  <div className="input-group form-group mt-3">
+                    <div className="bg-secondary rounded-start">
+                      <span className="m-3"><i className="fas fa-user mt-2"></i></span>
                     </div>
+                    <input type="text" className="form-control" placeholder="Usuario" name="username" />
                   </div>
+                  <div className="input-group form-group mt-3">
+                    <div className="bg-secondary rounded-start">
+                      <span className="m-3"><i className="fas fa-key mt-2"></i></span>
+                    </div>
+                    <input type="password" className="form-control" placeholder="Contraseña" name="password" />
+                  </div>
+                  <div className="form-group mt-3">
+                    <input type="submit" value="Acceder" className="btn bg-secondary float-end text-white w-100" name="login-btn" />
+                  </div>
+                </form>
+              </div>
+              <div className="card-footer">
+                <div className="d-flex justify-content-center">
+                  <div className="text-primary">Si es un usuario registrado, inicie sesión aquí.</div>
                 </div>
               </div>
             </div>
           </div>
-        </section>
+        </div>
+      </>
     );
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,12 @@
 import { Route, Routes}  from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import DashboardPage from "../pages/DashboardPage";
-import Navbar from "../Navbar";
 
 export default function AppRouter() {
     return <>
         <Routes>
-            <Route path="/" element={<Navbar/>}>
-                <Route index element={<LoginPage/>} />
-                <Route path="dashboard" element={<DashboardPage/>} />
-            </Route> 
+            <Route index element={<LoginPage/>} />
+            <Route path="dashboard" element={<DashboardPage/>} />
         </Routes>
     </>;
 }
