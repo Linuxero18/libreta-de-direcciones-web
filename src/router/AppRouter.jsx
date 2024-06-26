fbf22afdbb825c915e9ec001216503700214d9dc
AntonyHq <ln60158768@idat.edu.pe>
2024-06-26 15:33:08 -0500

    ruta privada para pagina admin

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,35 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import DashboardPage from "../pages/DashboardPage";
+import AdminPage from "../pages/AdminPage";
 import PrivateRoute from "./PrivateRoute";
 import React, { useState } from "react";
 
 export default function AppRouter() {
   const [estaAutenticado, setEstaAutenticado] = useState(false);
+  
   return (
     <Routes>
+
       <Route
         path="/"
         element={<LoginPage setEstaAutenticado={setEstaAutenticado} />}
       />
+
       <Route
         path="/login"
         element={<LoginPage setEstaAutenticado={setEstaAutenticado} />}
       />
+
+      <Route
+        path="/admin"
+        element={
+          <PrivateRoute estaAutenticado={estaAutenticado}>
+            <AdminPage />
+          </PrivateRoute>
+         }
+        />
+        
       <Route
         path="/dashboard"
         element={
@@ -24,6 +38,7 @@ export default function AppRouter() {
           </PrivateRoute>
         }
       />
+
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
   );
