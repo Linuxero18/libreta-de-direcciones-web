07d22ab2ada6425df4f00ea043037625e235a212
Linuxero18 <piterm555perez@gmail.com>
2024-06-27 18:11:52 -0500

    commit nuevo

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -19,7 +19,7 @@ export default function DashboardPage() {
         console.log("No se encontró el RUC");
         return;
       }
-      console.log("Ruc ingresado: " + ruc);
+      console.log("RUC: " + ruc);
       try {
         const response = await axios.get(
           `http://localhost:4000/clientes/ruc/${ruc}`
@@ -61,7 +61,15 @@ export default function DashboardPage() {
     setFormularioData(prevState => ({ ...prevState, [name]: value }));
   };
 
-  const Guardar = async (e) => {
+  const rolChange = (e) => {
+    const rol = e.target.value;
+    setFormularioData(prevState => ({ ...prevState, ROL: rol }));
+    console.log("Rol seleccionado: ", rol);
+  };
+
+  const rolesDisponibles = ["Atencion de alertas", "Control de cambio", "Facturacion"];
+
+  const GuardarRegistrar = async (e) => {
     e.preventDefault();
 
     const datosActualizados = {
@@ -72,21 +80,45 @@ export default function DashboardPage() {
       whatsapp: formularioData.WHATSAPP,
       rol: formularioData.ROL
     };
-    
+
+    console.log("Datos actualizados: ", datosActualizados);
+
     try {
-      await axios.put(`http://localhost:4000/clientes/${servicioSeleccionado}`, datosActualizados, {
+      const response = await axios.put(`http://localhost:4000/clientes/${servicioSeleccionado}`, datosActualizados, {
         headers: {
           'Content-Type': 'application/json'
         }
       });
-      setError("El servicio se ha actualizado correctamente.");
+
+      const responseHistorial = await axios.post(`http://localhost:4000/historial`, {
+        abonado: servicioSeleccionado,
+        ...datosActualizados,
+        fu_modificacion: new Date().toISOString()
+      });
+
+      setError("El servicio "+ servicioSeleccionado +" se ha actualizado correctamente.");
       setMostrarFormulario(false);
       await obtenerServicios();
+      console.log("DATA:" + JSON.stringify(formularioData));
+      console.log("Respuesta del servidor:", response.data); 
+      console.log("Respuesta del servidor tabla historial:", responseHistorial.data);
     } catch (e) {
       setError("Error al actualizar el servicio.");
       console.log(e);
     }
   }
+
+  const Cancelar = () => {
+    setFormularioData({
+      NOMBRE: '',
+      CORREO: '',
+      CONTACTO: '',
+      TELEFONO: '',
+      WHATSAPP: '',
+      ROL: ''
+    });
+    setMostrarFormulario(false);
+  };
 
   return (
     <div className="container">
@@ -94,6 +126,7 @@ export default function DashboardPage() {
       <table className="table table-hover table-bordered" key={servicios.length}>
         <thead className="thead-dark"> 
           <tr>
+            <th>Nº</th>
             <th>ABONADO</th>
             <th>NOMBRE</th>
             <th>CORREO</th>
@@ -107,6 +140,7 @@ export default function DashboardPage() {
         <tbody>
           {servicios.map((servicio, index) => (
             <tr key={index}>
+              <td>{index + 1}</td>
               <td>{servicio.ABONADO}</td>
               <td>{servicio.NOMBRE}</td>
               <td>{servicio.CORREO}</td>
@@ -124,40 +158,56 @@ export default function DashboardPage() {
       </table>
       <div className="d-grid gap-2">
         <div className="footer">
-        {error && <div className="text-danger mt-3" align="left">{error}</div>}
-        <button className="btn btn-primary" type="button" onClick={Actualizar}>Actualizar</button>
+          {error && <div className="text-danger mt-3" align="left">{error}</div>}
+          <div className="group-btn">
+            <button className="btn btn-secondary" type="button">Detalles</button>
+            <button className="btn btn-primary" type="button" onClick={Actualizar}>Actualizar</button>
+          </div>
         </div>
       </div>
-      {mostrarFormulario && (
-        <form onSubmit={Guardar}>
-          <h2 className="mt-5 mb-4">Actualización de datos del RUC {formularioData.RUC}</h2>
-          <div className="mb-3">
-            <label htmlFor="nombre" className="form-label">Nombre:</label>
-            <input type="text" className="form-control" id="nombre" name="NOMBRE" value={formularioData.NOMBRE || ''} onChange={InputData} required/>
+      <div className="container2" align="center">
+        {mostrarFormulario && (
+          <div className="card">
+            <div class="headerform">DATOS DEL SERVICIO {formularioData.ABONADO} - {formularioData.RUC}</div>
+              <form onSubmit={GuardarRegistrar}>
+                <div className="mb-3">
+                  <label htmlFor="nombre" className="form-label">Nombre:</label>
+                  <input type="text" className="form-control" id="nombre" name="NOMBRE" value={formularioData.NOMBRE || ''} onChange={InputData} required/>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="correo" className="form-label">Correo:</label>
+                  <input type="email" className="form-control" id="correo" name="CORREO" value={formularioData.CORREO || ''} onChange={InputData} required/>
+                </div>  
+                <div className="mb-3">
+                  <label htmlFor="contacto" className="form-label">Contacto:</label>
+                  <input type="text" className="form-control" id="contacto" name="CONTACTO" value={formularioData.CONTACTO || ''} onChange={InputData} required/>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="telefono" className="form-label">Teléfono:</label>
+                  <input type="number" className="form-control spin" id="telefono" name="TELEFONO" value={formularioData.TELEFONO || ''} onChange={InputData} required onKeyDown={(e) => e.key === 'ArrowUp' || e.key === 'ArrowDown' ? e.preventDefault() : null}/>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="whatsapp" className="form-label">WhatsApp:</label>
+                  <input type="number" className="form-control spin" id="whatsapp" name="WHATSAPP" value={formularioData.WHATSAPP || ''} onChange={InputData} required onKeyDown={(e) => e.key === 'ArrowUp' || e.key === 'ArrowDown' ? e.preventDefault() : null}/>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="rol" className="form-label">Rol:</label>
+                  <select className="form-control" id="rol" name="ROL" value={formularioData.ROL}  onChange={rolChange}>
+                    {rolesDisponibles.map((rol, index) => (
+                      <option key={index} value={rol}>{rol}</option>
+                    ))}
+                  </select>
+                </div>
+                <div className="footer">
+                  <button type="button" className="btn btn-danger btnform" onClick={Cancelar}>Cancelar</button>
+                  <button type="submit" className="btn btn-primary btnform" disabled={cargando}>{cargando ? 'Guardando ...' : 'Guardar'}</button>
+                </div>
+              </form>
           </div>
-          <div className="mb-3">
-            <label htmlFor="correo" className="form-label">Correo:</label>
-            <input type="email" className="form-control" id="correo" name="CORREO" value={formularioData.CORREO || ''} onChange={InputData} required/>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="contacto" className="form-label">Contacto:</label>
-            <input type="text" className="form-control" id="contacto" name="CONTACTO" value={formularioData.CONTACTO || ''} onChange={InputData} required/>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="telefono" className="form-label">Teléfono:</label>
-            <input type="text" className="form-control" id="telefono" name="TELEFONO" value={formularioData.TELEFONO || ''} onChange={InputData} required/>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="whatsapp" className="form-label">WhatsApp:</label>
-            <input type="text" className="form-control" id="whatsapp" name="WHATSAPP" value={formularioData.WHATSAPP || ''} onChange={InputData} required/>
-          </div>
-          <div className="mb-3">
-            <label htmlFor="rol" className="form-label">Rol:</label>
-            <input type="text" className="form-control" id="rol" name="ROL" value={formularioData.ROL || ''} onChange={InputData} required/>
-          </div>
-          <button type="submit" className="btn btn-primary" disabled={cargando}>{cargando ? 'Guardando ...' : 'Guardar'}</button>
-        </form>
-      )}
+        )}
+      </div>
+      <div className='container3'>
+      </div>
     </div>
   );
 }
